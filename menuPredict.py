6150de9aae061379c7888f13f7b7d76377e0d862
SungHo <itssungho17@gmail.com>
2020-10-13 22:51:42 +0900

    menuPredict input mode

--- menuPredict.py
+++ menuPredict.py
@@ -7,19 +7,17 @@ model = FastText.load(model_file)
 # print(len(model.wv.vocab.keys()))
 # print(list(model.wv.index2word))
 
-searchText = '파스타'
-
-start = time.time()
-mostSimilarList = model.wv.most_similar(searchText, topn=10)
-end = time.time() - start
-
-print('=========================================')
-print('검색어 > ', searchText)
-print('=========================================')
-count = 0
-for i in mostSimilarList:
-    count += 1
-    print('{0}:\t{2:0.5f}:\t{1}'.format(count, i[0], i[1]))
-print('=========================================')
-print('predict time:\t{0:0.5f}'.format(end))
-print('=========================================')
+while (1):
+    print('=========================================')
+    searchText = input('검색어를 입력하세요 > ')
+    start = time.time()
+    mostSimilarList = model.wv.most_similar(searchText, topn=10)
+    end = time.time() - start
+    print('=========================================')
+    print('검색어 > ', searchText)
+    print('predict time:\t{0:0.5f}'.format(end))
+    print('=========================================')
+    count = 0
+    for i in mostSimilarList:
+        count += 1
+        print('{0}:\t{2:0.5f}:\t{1}'.format(count, i[0], i[1]))
